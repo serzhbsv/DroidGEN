a3d697e4fe2cd639cb9b38f1eec8214d15aef653
Serzh5 <sepzh@rambler.ru>
2026-05-24 00:36:25 +0300

    fix

--- CompileDroidGen.bat
+++ CompileDroidGen.bat
@@ -1,5 +1,5 @@
 SET JAVA_HOME=F:\JAVA\openjdk\openjdk-1.8.0_x64
 SET PATH=%PATH%;F:\JAVA\ant\bin;F:\JAVA\ant\lib;F:\JAVA\openjdk\openjdk-1.8.0_x64\bin;F:\Android\ndk\android_ndk_r8_crystax
-call ndk-build clean
+call ndk-build  -V=1
 call ant debug
 pause
