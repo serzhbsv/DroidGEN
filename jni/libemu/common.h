3415f04ee2f5fbbf27e71501c0be0483789dc53f
serzhbsv <sepzh@rambler.ru>
2026-07-17 12:59:25 +0300

    Заменён CLOCK_REALTIME_HR на CLOCK_REALTIME

--- jni/libemu/common.h
+++ jni/libemu/common.h
@@ -62,7 +62,7 @@
 static double now_ms(void)
 {
     struct timespec res;
-    clock_gettime(CLOCK_REALTIME_HR, &res);
+    clock_gettime(CLOCK_REALTIME, &res);
     return 1000.0*res.tv_sec + (double)res.tv_nsec/1e6;
 }
 
