bc314844768de4a8740d9f1d593be94f7fa73b74
serzhbsv <sepzh@rambler.ru>
2026-07-19 19:44:59 +0300

    испровления не игрузит файлы roms

--- jni/Application.mk
+++ jni/Application.mk
@@ -8,7 +8,7 @@ APP_OPTIM := release
 #APP_STL := gnustl_static
 #APP_STL := stlport_static
 
-APP_ABI := armeabi-v7a arm64-v8a
+APP_ABI := armeabi-v7a
 APP_PLATFORM := android-19
 APP_CFLAGS := -std=c99 -O0 -g -fPIC -w
 APP_CPPFLAGS := -std=c++11 -O0 -g -fPIC -w
